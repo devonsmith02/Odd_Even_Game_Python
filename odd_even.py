3d72077f9f6eb94c799d944416b902757c80f9e3
Priyanshu Tiwari <priyanshutiwari786@gmail.com>
2018-10-28 01:12:49 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -1,16 +1,16 @@
 from random import randint
-print('what is your name?')
+print('what is your name?')#enters the username
 name = input()
 print('Hello, ' + name + ' I am Priyanshu here lets play odd even cricket')
 print('I hope you are well aware of the rules of the game let me make it clear that you can only score runs between 1 and 6')
 print('Time for the toss')
-call = input("'even','odd'?")
+call = input("'even','odd'?")#PLayer makes his choice
 print('enter toss number')
 toss = int(input())
-compu = randint(1,7)
+compu = randint(1,7)#give a random number to the user
 player = 0
 comp =-1
-if ((toss + compu) % 2 == 0 and call == 'even'):
+if ((toss + compu) % 2 == 0 and call == 'even'):#checks the winner of toss
     print('You won what would you like to do')
     choice = input("batting,bowling?")
 elif ((toss + compu) % 2 != 0 and call == 'odd'):
@@ -24,7 +24,7 @@ if(choice == 'batting'):
     print('You chose to bat start now:')
     score = 0
     while comp != player:
-        player = int(input())
+        player = int(input())#get input choice of user
         if(player > 6 or player < 0):
             print('that is an invalid choice')
             continue
@@ -70,7 +70,7 @@ if(choice == 'bowling'):
     while comp != player and score <= scorec:
         player = int(input())
         if(player > 6 or player < 0):
-            print('that is an invalid choice')
+            print('that is an invalid choice')#print the invalid choice message
             continue
         comp = randint(1,6)
         if(player!=comp):
